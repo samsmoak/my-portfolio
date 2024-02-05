09ffc0366536cc60c1cadc4b1c1b9bcbf33aa0b1
samuelzih <samuelz@instaacoders.com>
2024-02-05 20:00:16 +0530

    second commit 3.2

--- src/components/ProfileSection/ProfileSectioncomponents/Description.js
+++ src/components/ProfileSection/ProfileSectioncomponents/Description.js
@@ -10,7 +10,7 @@ function Description() {
 	};
 	return (
 		<div className='space-y-4 px-4   text-neutral-700 w-full flex flex-col items-center   lg:items-start '>
-			<div className='  w-full items-center  md:w-full flex flex-col md:items-start  md:justify-start '>
+			<div className='w-full   items-center  md:w-full flex flex-col md:items-start  md:justify-start '>
 				<div className='text-sm md:text-2xl capitalize '>
 					Hi, I'm <span className='font-semibold '>jeff edmonds</span> A
 				</div>
